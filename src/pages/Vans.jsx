cb964d92c478029df32a7860abed2b7da8810b6c
CandyTale55 <candytale55@gmail.com>
2026-02-25 21:37:39 +0100

    feat(vans): fetch and display van data and images from API using MirageJS as mockup.

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,17 +1,28 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Vans() {
+
+  const [vans, setVans] = useState([]);
 
   useEffect(() => {
     fetch("/api/vans")
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => setVans(data.vans));
   }, []);
+
+  const vanElements = vans.map(van => 
+    <div key={van.id}>
+      <img src={van.imageUrl} alt={van.name} />
+      <div>
+        <h3>{van.name}</h3>
+        <p>{van.description}</p>
+      </div>
+    </div>
+  );
 
   return (
     <>
-      <h2>I am Vans</h2>
-
+      <div>{ vanElements}</div>
     </>
   )
 }
