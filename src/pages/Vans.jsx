81ce870ceb2c0c3e69c61a9c3dd276ab055d461a
CandyTale55 <candytale55@gmail.com>
2026-02-25 20:30:22 +0100

    feat(vans): fetch and display van data from mock API docs

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useEffect } from 'react'
 
 export default function Vans() {
+
+  useEffect(() => {
+    fetch("/api/vans")
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
+
   return (
-    <h2>I am Vans</h2>
+    <>
+      <h2>I am Vans</h2>
+
+    </>
   )
 }
