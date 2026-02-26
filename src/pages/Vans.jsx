a0fcca0a2f871bd8bd7d7aa8be929823d770e023
CandyTale55 <candytale55@gmail.com>
2026-02-26 05:56:38 +0100

    feat(vans): wrap van elements in Link for navigation to detail page

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Vans() {
 
@@ -11,14 +12,17 @@ export default function Vans() {
   }, []);
 
   const vanElements = vans.map(van => 
-    <div key={van.id}>
-      <img src={van.imageUrl} alt={van.name} />
-      <div>
-        <h3>{van.name}</h3>
-        <p>€{van.price}</p>
+    <Link key={van.id} to={`/vans/${van.id}`}>
+      <div key={van.id}>
+        <img src={van.imageUrl} alt={van.name} />
+        <div>
+          <h3>{van.name}</h3>
+          <p>€{van.price}</p>
+        </div>
+        <i>{van.type}</i>
       </div>
-      <i>{van.type}</i>
-    </div>
+    </Link>
+    
   );
 
   return (
