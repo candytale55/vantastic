4011b54cc08c36b059ea66a40b6ee4f4ec17aee0
CandyTale55 <candytale55@gmail.com>
2026-02-27 18:50:23 +0100

    feat(VanCard): implement reusable VanCard component and update Vans to use it

--- src/components/VanCard.jsx
+++ src/components/VanCard.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function VanCard() {
+export default function VanCard(props) {
+
+  console.log("Card received:", props.van); //TODO: Delete after testing is done
+  
   return (
-    <h2>I am VanCard</h2>
+    <Link key={props.van.id} to={`/vans/${props.van.id}`}>
+      <div key={props.van.id}>
+        <img src={props.van.imageUrl} alt={props.van.name} />
+        <div>
+          <h3>{props.van.name}</h3>
+          <p>€{props.van.price}</p>
+        </div>
+        <i>{props.van.type}</i>
+      </div>
+    </Link>
   )
 }
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,5 +1,5 @@
 import useFetch from '../hooks/useFetch.jsx'
-import { Link } from 'react-router-dom'
+import VanCard from '../components/VanCard.jsx'
 
 
 export default function Vans() {
@@ -11,16 +11,7 @@ export default function Vans() {
   if (error) return <h2>Hubo un error:{error}</h2> //TODO: Improve error message styling (it sucks!)
 
   const vanElements = data.vans.map(van => {
-    return <Link key={van.id} to={`/vans/${van.id}`}>
-      <div key={van.id}>
-        <img src={van.imageUrl} alt={van.name} />
-        <div>
-          <h3>{van.name}</h3>
-          <p>€{van.price}</p>
-        </div>
-        <i>{van.type}</i>
-      </div>
-    </Link>
+    return <VanCard key={van.id} van={van} />
   })
 
   return (
