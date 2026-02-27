59418be9bd286347cb25baa691920e4b0f50b64c
CandyTale55 <candytale55@gmail.com>
2026-02-27 16:56:33 +0100

    feat(VanDetail): load van with correct API endpoint

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -5,17 +5,26 @@ export default function VanDetail() {
   // Extrae el parámetro "id" de la URL 
   const params = useParams();
 
-  const { data, loading, error } = useFetch(`/api/vans-error/${params.id}`);
-
-  console.log("Current data state:", data); // TODO: Delete after tests are done.
+  const { data, loading, error } = useFetch(`/api/vans/${params.id}`);
 
   if (loading) return <h2>Cargando vans...</h2>
 
   if (error) return <h2>Hubo un error:{error}</h2> //TODO: Improve error message styling (it sucks!)
-  
+
+  const vanElement = data.van;
+
   return (
     <>
-      <h2>I am VanDetail</h2>
+      
+      <h2>{vanElement.name}</h2>
+      <img src={vanElement.imageUrl} alt={vanElement.name} />
+      <div>
+        <i>{vanElement.type}</i>
+        <h3>{vanElement.name}</h3>
+        <p>{vanElement.description}</p>
+        <p><strong>Precio:</strong> €{vanElement.price} por día</p>
+      </div>
+      <button className="cta">Alquila esta van!</button>
     </>
   )
 }
