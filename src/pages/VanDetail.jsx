ef79a1e03e0f8cd4f6450abe84904703f1e50486
CandyTale55 <candytale55@gmail.com>
2026-02-27 16:35:44 +0100

    refactor(VanDetail): simplify data fetching by using useFetch hook and remove unused state

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,38 +1,31 @@
-import { useEffect, useState } from 'react'
+import useFetch from '../hooks/useFetch'
 import { useParams } from 'react-router-dom'
 
 export default function VanDetail() {
   // Extrae el parámetro "id" de la URL 
   const params = useParams();
 
-  // Almacena los datos de la van (inicia undefined)
-  const [vanElement, setVanElement] = useState();
-  // TODO: Check if you want to set initial useState to null and conditionally render a van (Ref: https://scrimba.com/advanced-react-c02h/~02l)
+  const { data, loading, error } = useFetch(`/api/vans/${params.id}`);
 
-  // Llama a la API de Mirage y guarda la van en el estado. Se ejecuta al montar el componente o cuando cambia params.id
-  useEffect(() => {
-    fetch(`/api/vans/${params.id}`)  
-      .then(res => res.json())
-      .then(data => setVanElement(data.van))  // Mirage envuelve la respuesta en { van: {...} }
-  }, [params.id]);
+  console.log("Current data state:", data); // TODO: Delete after tests are done.
 
-  // Muestra mensaje de carga mientras no haya datos (evitar error cuando vanElement aún es undefined)
-  if (!vanElement) {
-    return <h2>Cargando van ...</h2>
-  }
+  /*
+  Current data state: 
+    {van: {…}}
+    van
+    : 
+    {id: '2', name: 'Beach Bum', price: 80, description: 'Beach Bum es una van inspirada en surfistas y viaj… geniales que no encontrarás en una camper común.', imageUrl: 'https://images.unsplash.com/photo-1710707919147-bf41f37e5b00?&w=400&h=400&fit=crop', …}
+    [[Prototype]]
+    : 
+    Object
+   */
 
-  // Cargados los datos, renderiza el detalle de la van
+
+
+  
   return (
     <>
-      <h2>{vanElement.name}</h2>
-      <img src={vanElement.imageUrl} alt={vanElement.name} />
-      <div>
-        <i>{vanElement.type}</i>
-        <h3>{vanElement.name}</h3>
-        <p>{vanElement.description}</p>
-        <p><strong>Precio:</strong> €{vanElement.price} por día</p>
-      </div>
-      <button className="cta">Alquila esta van!</button>
+      <h2>I am VanDetail</h2>
     </>
   )
 }
