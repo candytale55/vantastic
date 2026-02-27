8b5c6432cd73da2ccab23d166c982dd558b0c314
CandyTale55 <candytale55@gmail.com>
2026-02-27 21:06:50 +0100

    feat(VanCard): add favorite button functionality to VanCard component

--- src/components/VanCard.jsx
+++ src/components/VanCard.jsx
@@ -3,9 +3,16 @@ import { Link } from 'react-router-dom'
 export default function VanCard(props) {
 
   console.log("Card received:", props.van); //TODO: Delete after testing is done
+
+  const handleFavorite = (event) => { 
+    event.preventDefault(); 
+    event.stopPropagation();  // Stops the click from "bubbling" up to the card link
+    console.log("Favorited van ID:", props.van.id);
+   };
   
   return (
     <Link key={props.van.id} to={`/vans/${props.van.id}`}>
+      <button onClick={handleFavorite}> ❤️ </button>
       <div key={props.van.id}>
         <img src={props.van.imageUrl} alt={props.van.name} />
         <div>
