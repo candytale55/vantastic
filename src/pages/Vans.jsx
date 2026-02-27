38de310fcc4f692909c5d0e712759a616d6becc7
CandyTale55 <candytale55@gmail.com>
2026-02-27 13:28:35 +0100

    feat(vans): render van list from API data

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export default function Vans() {
 
-  const { data, loading, error } = useFetch("/api/vans-error");
+  const { data, loading, error } = useFetch("/api/vans");
 
   console.log("Current data state", data); //TODO: Remove after testing is over
 
@@ -14,14 +14,27 @@ export default function Vans() {
 
 
   if (error) {
-    console.log("Hubo un error:", { error });
-    return <h2>Hubo un error:{error}</h2>
+    console.log("Hubo un error:", { error }); // TODO: Remove after testing is over 
+    return <h2>Hubo un error:{error}</h2> //TODO: Improve error message styling (it sucks!)
   }
+
+  const vanElements = data.vans.map(van => {
+    return <Link key={van.id} to={`/vans/${van.id}`}>
+      <div key={van.id}>
+        <img src={van.imageUrl} alt={van.name} />
+        <div>
+          <h3>{van.name}</h3>
+          <p>€{van.price}</p>
+        </div>
+        <i>{van.type}</i>
+      </div>
+    </Link>
+  })
 
   return (
     <>
       <h2>Explora nuestros modelos</h2>
-{/*       <div>{ vanElements}</div> */}
+      <div>{ vanElements}</div>
     </>
   )
 }
