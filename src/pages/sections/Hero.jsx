b648127a484682073328b10fa808414cbec3ef73
CandyTale55 <candytale55@gmail.com>
2026-03-04 01:55:03 +0100

    feat(Hero): implement Hero section with content and buttons - SKELETON

--- src/pages/sections/Hero.jsx
+++ src/pages/sections/Hero.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 export default function Hero() {
-  return (
-    <h2>I am Hero</h2>
+    return (
+        <section className="section">
+            <p>Est. 1989 · Valencia</p>
+            <h2>TEXTOTOTE <span>Textote Rosa</span> TEXTOTOTE</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur provident obcaecati earum atque minima iusto temporibus? Consequatur fuga commodi dolor nemo ut, temporibus dolore et eligendi sit dolorem officiis architecto!</p>
+            {/* //TODO: Cambiar estos botones, deben ser links */}
+            <button>Ver la Flota</button>
+            <button>Únete al Movimiento</button>
+        </section>
   )
 }
