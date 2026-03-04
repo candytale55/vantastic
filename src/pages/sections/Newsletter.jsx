be16a027957e8b05db450d3500b6487e1a4fc93f
CandyTale55 <candytale55@gmail.com>
2026-03-04 02:00:39 +0100

    feat(Newsletter): add content to Newsletter section with structured layout and form - SKELETON

--- src/pages/sections/Newsletter.jsx
+++ src/pages/sections/Newsletter.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export default function Newsletter() {
-  return (
-    <h2>I am Newsletter</h2>
+    return (
+      <section className="section" id="newsletter-section">
+            <h2>I am Newsletter</h2>
+            <button>CON</button>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis error, distinctio cupiditate in suscipit eligendi aliquid dolore quos reprehenderit quibusdam consectetur facilis? Porro sit obcaecati, amet at eius similique fugit?</p>
+
+            <form>
+                <label htmlFor="newsletter-email" placeholder="tu dirección de correo"></label>
+                <input type="mail" id="newsletter-email" />
+                <button type="submit">Únete al Movimiento</button>
+            </form>
+      </section>
   )
 }
