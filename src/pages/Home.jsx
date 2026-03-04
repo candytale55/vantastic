9b55c0d5782495b680af912f0b83c035de98e257
CandyTale55 <candytale55@gmail.com>
2026-03-04 03:09:24 +0100

    feat(Home): remove introductory text and link from Home component - Left only the section components.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,6 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div>
-      <h2>Tú tienes el plan de viaje, nosotros tenemos la van perfecta.</h2>
-      <p>Dale aventura a tu vida y súmate al movimiento vanlife. Renta la van ideal para convertir tu road trip en el viaje que estás imaginando.</p>
-      <Link to="vans">Encuentra tu van</Link>
       <Hero />
       <Fleet />
       <Promo />
