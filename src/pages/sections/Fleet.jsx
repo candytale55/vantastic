4bc21e1cabb7ce3c0eb1de9acf57989e4defa130
CandyTale55 <candytale55@gmail.com>
2026-03-04 02:44:34 +0100

    feat(Fleet): update link text and add fleet gallery layout - SKELETON

--- src/pages/sections/Fleet.jsx
+++ src/pages/sections/Fleet.jsx
@@ -6,7 +6,14 @@ export default function Fleet() {
             <h2>La Flota</h2>
             <h3>Tú tienes el plan de viaje, nosotros tenemos la van perfecta.</h3>
             <p>Dale aventura a tu vida y súmate al movimiento vanlife. Renta la van ideal para convertir tu road trip en el viaje que estás imaginando.</p>
-            <Link to="vans">Encuentra tu van</Link>
+            <Link to="vans">Ver toda la Flota</Link>
+            <h4>Aquí van los 3 tipos de Vans</h4>
+            {/* //TODO: Cambiar por componente */}
+            <div className="fleet-gallery-container">
+                <div>Cuadro 1</div>
+                <div>Cuadro 2</div>
+                <div>Cuadro 3</div>
+            </div>
         </section>
     )
 }
