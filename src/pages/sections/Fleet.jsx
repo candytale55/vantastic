af0484618dde064773797bc33de578fb89b78f4c
CandyTale55 <candytale55@gmail.com>
2026-03-04 01:45:29 +0100

    feat(Fleet): add section structure and placeholder content for Fleet page

--- src/pages/sections/Fleet.jsx
+++ src/pages/sections/Fleet.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Fleet() {
-  return (
-    <h2>I am Fleet</h2>
-  )
+    return (
+        <section className="section" id="fleet-section">
+            <h2>La Flota</h2>
+            <h3>Tú tienes el plan de viaje, nosotros tenemos la van perfecta.</h3>
+            <p>Dale aventura a tu vida y súmate al movimiento vanlife. Renta la van ideal para convertir tu road trip en el viaje que estás imaginando.</p>
+            <Link to="vans">Encuentra tu van</Link>
+        </section>
+    )
 }
