767f441d3d9e486e88ffb2e36e9ee895656b629a
CandyTale55 <candytale55@gmail.com>
2026-03-04 01:21:11 +0100

    feat(Home): add new sections (Hero, Fleet, Promo, Newsletter, Social) - ONLY SKELETON

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import Hero from './sections/Hero.jsx'
+import Fleet from './sections/Fleet.jsx'
+import Promo from './sections/Promo.jsx'
+import Newsletter from './sections/Newsletter.jsx'
+import Social from './sections/Social.jsx'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
@@ -7,6 +11,11 @@ export default function Home() {
       <h2>Tú tienes el plan de viaje, nosotros tenemos la van perfecta.</h2>
       <p>Dale aventura a tu vida y súmate al movimiento vanlife. Renta la van ideal para convertir tu road trip en el viaje que estás imaginando.</p>
       <Link to="vans">Encuentra tu van</Link>
+      <Hero />
+      <Fleet />
+      <Promo />
+      <Newsletter />
+      <Social />
     </div>
   )
 }
