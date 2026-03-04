62aa1b733ed2cda623a57ac1d380f0a730762f8b
CandyTale55 <candytale55@gmail.com>
2026-03-04 03:01:32 +0100

    feat(Footer): Add content, layout and styling for footer section - SKELETON

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 //TODO:Add dynamic date in JS.
 export default function Footer() {
   return (
-    <footer>
+    <footer className="footer section">
+      <div className="inner-footer-container">
+        <div>
+          <div>
+            <h2>Footer</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat ut iure accusamus commodi reiciendis? Distinctio, quis. Saepe, magnam, quidem accusantium, quod corporis explicabo perspiciatis enim error sunt recusandae iusto repellat!</p>
+          </div>
+        </div>
+        <div>
+          <ul>
+            <h4>Enlaces rápidos</h4>
+            <li>Enlace</li>
+            <li>Enlace</li>
+            <li>Enlace</li>
+            <li>Enlace</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
       <h4>&copy; 2026 Vantastic!</h4>
     </footer>
   )
