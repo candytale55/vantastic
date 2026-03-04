001985afb1c9f8c47c2f2c590db4846be72802a0
CandyTale55 <candytale55@gmail.com>
2026-03-04 02:11:10 +0100

    feat(Social): add content to Social section - SKELETON

--- src/pages/sections/Social.jsx
+++ src/pages/sections/Social.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 export default function Social() {
-  return (
-    <h2>I am Social</h2>
+    return (
+      <section className="section" id="social-section">
+            <h2>Vantastic Moments (I am Social)</h2>
+            <div>Aqui van las fotos tipo galería</div>
+            <button>Síguenos en Vantastic!</button>
+            
+      </section>
   )
 }
