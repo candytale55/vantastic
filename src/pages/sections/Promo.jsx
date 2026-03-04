bde1f544aff7f25e28ffcd18da6830aec321394e
CandyTale55 <candytale55@gmail.com>
2026-03-04 02:22:52 +0100

    feat(Promo): add promo section with layout and content - SKELETON

--- src/pages/sections/Promo.jsx
+++ src/pages/sections/Promo.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export default function Promo() {
-  return (
-    <h2>I am Promo</h2>
+    return (
+      <section className="section" id="promo-section">
+            <div className="promo-container">
+                <i>icon</i>
+                <h2>I<span> am</span> promo</h2>
+            </div>
+            <div className="promo-container">
+                <i>icon</i>
+                <p>text<span> text</span></p>
+            </div><div className="promo-container">
+                <i>icon</i>
+                <p>text<span> text</span></p>
+            </div>
+      </section>
   )
 }
