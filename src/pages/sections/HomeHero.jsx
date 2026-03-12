bebdfe0c24f15f0587ff5adf63dd76f12a513afc
CandyTale55 <candytale55@gmail.com>
2026-03-12 09:46:39 +0100

    feat(HomeHero): update HomeHero section with new content - add placeholder image and placeholder for video

--- src/pages/sections/HomeHero.jsx
+++ src/pages/sections/HomeHero.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import placeholder_image from '/src/assets/images/placeholder_image.jpg'
+import { Link } from 'react-router-dom'
 
 export default function HomeHero() {
     return (
-        <section className="section">
-            <p>Est. 1989 · Valencia</p>
-            <h2>TEXTOTOTE <span>Textote Rosa</span> TEXTOTOTE</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur provident obcaecati earum atque minima iusto temporibus? Consequatur fuga commodi dolor nemo ut, temporibus dolore et eligendi sit dolorem officiis architecto!</p>
-            {/* //TODO: Cambiar estos botones, deben ser links */}
-            <button>Ver la Flota</button>
-            <button>Únete al Movimiento</button>
+        <section className="home-hero-section">
+
+            <div className="hero-background-video">
+                {/* //TODO: Video or no Video? */}
+                {/*                 <video autoplay muted loop playsinline class="video-element">
+                    <source src="/path/to/your/vanlife-video.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video> */}
+                <img className="placeholder-image" src={placeholder_image} alt="placeholder image" />
+            </div>
+
+            <div class="hero-content">
+                <h1>Treinta y cinco años llevando gente a ningún sitio en concreto.</h1>
+                <p class="subheadline">Furgonetas vintage restauradas. Recógela aquí. Devuélvela donde quieras.</p>
+                <Link to="/vans" class="cta-button-large">Encuentra tu furgoneta</Link>
+                <p class="brand-signature">Valencia, desde 1989. Familia. Carretera. Nada más.</p>
+            </div>
+            
+           
         </section>
     )
 }
