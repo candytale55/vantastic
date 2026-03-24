1cf45ba5b92aae2808557af99756aaf1c2a55727
CandyTale55 <candytale55@gmail.com>
2026-03-24 10:04:57 +0100

    feat: add base styling to About Us section with TailwindCSS

--- src/pages/sections/HomeAboutUs.jsx
+++ src/pages/sections/HomeAboutUs.jsx
@@ -2,13 +2,19 @@ import placeholder_image from '/src/assets/images/placeholder_image.jpg'
 
 export default function HomeAboutUs() {
     return (
-        <section className="home-about-hook-section section-shell">
-            <div className="about-hook-content">
-                <h2>Crecimos en una furgoneta. Abrimos esto para que tú puedas sentir lo mismo.</h2>
-                <p>La historia de Vantastic! no es la de una empresa. Es la de dos familias que llevan la carretera en la sangre desde los años 60. Es la historia de cómo la libertad se convirtió en un negocio honesto.</p>
-            </div>
-            <div className="about-image-home">
-                <img src={placeholder_image} alt="Foto de los fundadores o taller Vantastic!" />
+        <section className="home-about-hook section-shell">
+            <div className="about-layout">
+                <div className="about-content">
+                    <h2 className="about-title">Crecimos en una furgoneta. Abrimos esto para que tú puedas sentir lo mismo.</h2>
+                    <p className="about-text">La historia de Vantastic! no es la de una empresa. Es la de dos familias que llevan la carretera en la sangre desde los años 60. Es la historia de cómo la libertad se convirtió en un negocio honesto.</p>
+                </div>
+
+                <div className="about-media">
+                    <img
+                        src={placeholder_image}
+                        alt="Foto de los fundadores o taller Vantastic!"
+                        className="about-image" />
+                </div>
             </div>
         </section>
     )
