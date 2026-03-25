9ed8170ac552c27b2328442d5dfd625c2d6dcdf9
CandyTale55 <candytale55@gmail.com>
2026-03-25 22:20:27 +0100

    feat: update Header component styling and layout with TailwindCSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,18 @@ export default function Header() {
 
   return (
     <header
-      className="main-header mt-4 mx-4 flex flex-row items-center justify-between shrink-0">
+      className="main-header">
       <NavLink
           to="/"
-          className="navlink">
+          className="brand-link">
         <h1 className="brand-logo">Vantastic!</h1>
       </NavLink>
       
       <nav className="primary-nav">
         <NavLink
           to="vans"
-          className="navlink cta-button">¡Alquila tu Van!</NavLink>
+          className="header-cta">¡Alquila tu Van!</NavLink>
       </nav>
-
-
-      </header>
+    </header>
   ) 
 }
