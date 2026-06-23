39a66cff1644a3d4f13eeb8778e27fd74e17daa4
CandyTale55 <candytale55@gmail.com>
2026-06-23 20:49:11 +0200

    style: apply button styling to "Volver al inicio" link in NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div>
       <h2>404. Esta página no existe</h2>
-      <Link to="/">Volver al inicio</Link>
+      <Link to="/" className="cta-button">Volver al inicio</Link>
     </div>
   )
 }
