4d288c852373a1191a0254a1a4176aeef2ab276a
CandyTale55 <candytale55@gmail.com>
2026-06-23 20:56:15 +0200

    refactor: remove unused imports and temporary styles from App and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 // Components
 import Layout from './components/Layout.jsx'
 // Pages
-import About from '../docs/old/plan/About.jsx'
 import Home from './pages/Home.jsx'
 import NotFound from './pages/NotFound.jsx'
 import VanDetail from './pages/VanDetail.jsx'
 import Vans from './pages/Vans.jsx'
-import Community from '../docs/old/plan/Community.jsx'
-import RoutesPage from '../docs/old/plan/RoutesPage.jsx'
-import FAQ from '../docs/old/plan/FAQ.jsx'
-import Contact from '../docs/old/plan/Contact.jsx'
-import Legal from '../docs/old/plan/Legal.jsx'
-import Privacy from '../docs/old/plan/Privacy.jsx'
 
 import VanRatings from './pages/sections/VanRatings.jsx'
 import VanSpecs from './pages/sections/VanSpecs.jsx'
@@ -27,10 +20,7 @@ export default function App() {
       <Route element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="home" element={<Home />} />
-        <Route path="about" element={<About />} />
         <Route path="vans" element={<Vans />} />
-        <Route path="community" element={<Community />} />
-        <Route path="routes" element={<RoutesPage />} />
         <Route path="vans/:id" element={<VanDetail />}>
           <Route index element={<VanSpecs />} />
           <Route path="specs" element={<VanSpecs />} />
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,4 +8,4 @@ export default function NotFound() {
       <Link to="/" className="cta-button">Volver al inicio</Link>
     </div>
   )
-}
+}  
